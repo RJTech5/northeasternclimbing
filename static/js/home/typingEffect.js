ebd71b3ae006ccb9676328331a92afa1dc45caf5
RJTech5 <rjfieldstech@gmail.com>
2025-04-20 18:47:43 -0400

    Added home page.

---
+++ static/js/home/typingEffect.js
@@ -0,0 +1,17 @@
+// Final text to display.
+let finalText = "Northeastern University's Recreational Climbing Club";
+// Time delay between each character in milliseconds.
+let timeDelay = 50;
+
+let textBox = document.getElementById("titleText");
+textBox.innerHTML = "";
+
+for (let index = 0; index < finalText.length; index++) {
+    setTimeout(() => {addLetter(finalText[index]);}, timeDelay * index);
+}
+
+// Adds character to text box inner html.
+function addLetter(letter) {
+    textBox.innerHTML += letter;
+}
+
