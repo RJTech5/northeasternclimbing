5b8431c9f389dfe2eeb9d6baa50f7909c25383f0
RJTech5 <74117583+RJTech5@users.noreply.github.com>
2025-05-06 08:26:34 -0400

    Added climbing team modal

--- static/js/teamModal.js
+++ static/js/teamModal.js
@@ -1,16 +1,19 @@
 let MODAL = `<div id="teamOverlay" class="modal-overlay" style="display: none;">
   <div class="modal-content">
     <span class="close-btn" onclick="closeModal()">&times;</span>
-    <h2>Northeastern Club Climbing Team</h2>
+    <h2 style="font-family: 'Roboto', sans-serif;">Northeastern Club Climbing Team</h2>
     <div class="row" style="background: none; box-shadow: none;">
-        <div class="column" style="width: 50%;">
-        <p>This is your modal content.</p>
+        <div class="column">
+        <p style="font-size: 1.4em;">Looking for a structured, competitive climbing team environment? Check out Northeastern University’s club
+         climbing team! Northeastern club climbing meets regularly to practice, compete in regional and national
+          climbing competitions, and hosts tryouts in the Fall and Spring semesters. <a href="https://northeasternclimbing.github.io/team/"
+           target="_blank">Visit their website here.</a></p>
         </div>
         <div class="column" style="width: 50%;">
         <img src="../../static/images/climbing-team.webp" style="width: 100%">
         </div>
      </div>
-     <button onclick="closeModal()" class="homeJoinButton" style="width: 10vw; height: 5vh; font-size: 1em;">Back to NRC</button>
+     <button onclick="closeModal()" class="homeJoinButton" style="width: 10vw; height: 5vh; min-width: 100px; min-height: 50px; font-size: 1em;">Back to NRC</button>
 </div>
 </div>`;
 
